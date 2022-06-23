af3280dbc06c3e6439ebefbf454d3df1a15f476f
wi21b025 <wi21b025@technikum-wien.at>
2022-06-23 11:49:41 +0200

    Initial commit

---
+++ src/main/java/com/example/monitortest/MonitorTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorTestApplication.class, args);
+    }
+
+}
