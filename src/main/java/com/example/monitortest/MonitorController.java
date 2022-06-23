7f13594d0409820f65987eff6a8376bfe9b0272a
wi21b025 <wi21b025@technikum-wien.at>
2022-06-23 11:57:59 +0200

    .

---
+++ src/main/java/com/example/monitortest/MonitorController.java
@@ -0,0 +1,10 @@
+package com.example.monitortest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin //html zu greifen
+public class MonitorController {
+
+}
